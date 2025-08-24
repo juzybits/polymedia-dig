b81d9a62015460a317fb5b84ddd7585ffb63f2af
juzybits <96890594+juzybits@users.noreply.github.com>
2025-08-25 01:03:16 +0200

    digHole useMutation scaffolding

--- web/src/app/config.ts
+++ web/src/app/config.ts
@@ -1,11 +1,11 @@
 import type { ErrorsByPackage, NetworkName } from "@polymedia/suitcase-core";
 
-export const network: NetworkName = "devnet" as const;
+export const network: NetworkName = "localnet" as const;
 
 export const networkIds = {
 	mainnet: {
-		digPkgId: "0x01497e97d64aac81be68fc9cbfa7933d0c4b10dc39f4a85dda84ae2a774d36bc",
-		holeObjId: "0x987c912519b07c6348756a06cafdfaafb363dba15b79ab997beb36d72030607c",
+		digPkgId: "",
+		holeObjId: "",
 	},
 	testnet: {
 		digPkgId: "",
@@ -16,8 +16,8 @@ export const networkIds = {
 		holeObjId: "",
 	},
 	localnet: {
-		digPkgId: "",
-		holeObjId: "",
+		digPkgId: "0x01497e97d64aac81be68fc9cbfa7933d0c4b10dc39f4a85dda84ae2a774d36bc",
+		holeObjId: "0x987c912519b07c6348756a06cafdfaafb363dba15b79ab997beb36d72030607c",
 	},
 }[network];
 
--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -1,9 +1,46 @@
 // import { EarthVisualization } from "@/comp/Earth";
 
+import { useCurrentAccount } from "@mysten/dapp-kit";
+import { Transaction } from "@mysten/sui/transactions";
+import { Btn, Card, ConnectOr } from "@polymedia/suitcase-react";
+import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { useAppContext } from "@/app/context";
+
 export const PageHome = () => {
+	const currAcct = useCurrentAccount();
+	const { errParser, signAndExecuteTx, openConnectModal } = useAppContext();
+
+	const digHole = useMutation({
+		mutationFn: async () => {
+			if (!currAcct) {
+				throw new Error("Not connected");
+			}
+			const tx = new Transaction();
+			return signAndExecuteTx({ tx, sender: currAcct.address });
+		},
+		onSuccess: (resp) => {
+			toast.success("Success!");
+			console.log(`[dig] status:`, resp.effects?.status.status);
+			console.log(`[dig] digest:`, resp.digest);
+			console.log(`[dig] response:`, resp);
+		},
+		onError: (error) => {
+			console.warn(`[dig] error:`, error);
+			toast.error(errParser.errToStr(error, "Something went wrong"));
+		},
+	});
+
 	return (
 		<div className="page-regular">
 			<div className="page-title">I'm digging a hole</div>
+			<Card>
+				<ConnectOr openConnectModal={openConnectModal}>
+					<Btn onClick={() => digHole.mutate()} disabled={digHole.isPending}>
+						{digHole.isPending ? "DIGGING..." : "DIG HOLE"}
+					</Btn>
+				</ConnectOr>
+			</Card>
 			{/* <EarthVisualization /> */}
 		</div>
 	);
