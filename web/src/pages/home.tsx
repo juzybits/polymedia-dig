d9632a28ec6473c239f0ebbfa92339b738b187a9
juzybits <96890594+juzybits@users.noreply.github.com>
2025-08-25 02:34:11 +0200

    HoleDetails

--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -1,19 +1,40 @@
 // import { EarthVisualization } from "@/comp/Earth";
 
-import { useCurrentAccount } from "@mysten/dapp-kit";
+import { useCurrentAccount, useSuiClient } from "@mysten/dapp-kit";
 import { Transaction } from "@mysten/sui/transactions";
-import { Btn, Card, ConnectOr } from "@polymedia/suitcase-react";
-import { useMutation } from "@tanstack/react-query";
+import { objResToBcs } from "@polymedia/suitcase-core";
+import {
+	Btn,
+	Card,
+	CardDetail,
+	ConnectOr,
+	LinkToExplorer,
+} from "@polymedia/suitcase-react";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import toast from "react-hot-toast";
-import { networkIds } from "@/app/config";
+import { network, networkIds } from "@/app/config";
 import { useAppContext } from "@/app/context";
 import * as dig_module from "@/gen/dig/dig";
+import { Hole } from "@/gen/dig/dig";
 
 export const PageHome = () => {
+	const suiClient = useSuiClient();
 	const currAcct = useCurrentAccount();
 	const { errParser, signAndExecuteTx, openConnectModal } = useAppContext();
 
-	const digHole = useMutation({
+	const hole = useQuery({
+		queryKey: ["hole", networkIds.holeObjId],
+		queryFn: async () => {
+			return suiClient
+				.getObject({
+					id: networkIds.holeObjId,
+					options: { showType: true, showBcs: true },
+				})
+				.then((objRes) => Hole.fromBase64(objResToBcs(objRes)));
+		},
+	});
+
+	const dig = useMutation({
 		mutationFn: async () => {
 			if (!currAcct) {
 				throw new Error("Not connected");
@@ -45,12 +66,44 @@ export const PageHome = () => {
 			<div className="page-title">I'm digging a hole</div>
 			<Card>
 				<ConnectOr openConnectModal={openConnectModal}>
-					<Btn onClick={() => digHole.mutate()} disabled={digHole.isPending}>
-						{digHole.isPending ? "DIGGING..." : "DIG HOLE"}
+					<Btn onClick={() => dig.mutate()} disabled={dig.isPending}>
+						{dig.isPending ? "DIGGING..." : "DIG HOLE"}
 					</Btn>
 				</ConnectOr>
 			</Card>
+			<HoleDetails hole={hole.data} />
 			{/* <EarthVisualization /> */}
 		</div>
+	);
+};
+
+const HoleDetails = ({ hole }: { hole: typeof Hole.$inferType | undefined }) => {
+	const { explorer } = useAppContext();
+
+	if (!hole) return null;
+
+	const distanceKm = Number(hole.distance) / 1000;
+	const progressPct = (Number(hole.progress) / Number(hole.distance)) * 100;
+
+	return (
+		<Card>
+			<div className="card-title">Hole Facts</div>
+			<div className="card-details">
+				<CardDetail
+					label="ID"
+					val={
+						<LinkToExplorer
+							addr={hole.id.id}
+							kind="object"
+							explorer={explorer}
+							network={network}
+						/>
+					}
+				/>
+				<CardDetail label="Distance" val={`${distanceKm.toFixed(0)}km`} />
+				<CardDetail label="Progress" val={`${progressPct.toFixed(2)}%`} />
+				<CardDetail label="Diggers" val={hole.users.size} />
+			</div>
+		</Card>
 	);
 };
